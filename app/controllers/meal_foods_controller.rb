1d001c59b5f1f009e68d7029a32422cf40239a4b
Chien Wen Liu <dreamuk@gmail.com>
2019-05-17 13:40:13 +0800

    Add first problem’s solution

--- app/controllers/meal_foods_controller.rb
+++ app/controllers/meal_foods_controller.rb
@@ -1,6 +1,4 @@
 class MealFoodsController < ApplicationController
-  def no_meal_foods
-  end
 
   def max_foods
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  get "/no_meal_foods", to: "meal_foods#no_meal_foods"
+  get "/no_meal_foods", to: "meal#no_meal_foods"
   get "/max_foods", to: "meal_foods#max_foods"
 end
