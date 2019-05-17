8c9c2fe69fd089727267f6fed8d9c836283c2cc6
Chien Wen Liu <dreamuk@gmail.com>
2019-05-17 15:47:04 +0800

    Add problem 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get "/no_meal_foods", to: "meal#no_meal_foods"
   get "/max_foods", to: "food#max_foods"
+  get "/other_food", to: "food#other_food"
 end
